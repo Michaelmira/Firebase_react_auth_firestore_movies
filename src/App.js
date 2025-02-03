14a86288637ca7cbd8309c6f4c327194653e0076
Michael Mirisciotta <michaelmirisciotta@gmail.com>
2025-02-03 15:37:42 -0800

    Complete FireStore Addition

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { Auth } from './componets/auth.js';
-import { db } from './config/firebase.js';
-import { getDocs, collection, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import { db, auth, storage } from './config/firebase.js';
+import { getDocs, collection, addDoc, deleteDoc, updateDoc, doc } from 'firebase/firestore';
+import { ref, uploadBytes } from 'firebase/storage';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
@@ -15,16 +16,19 @@ function App() {
 
   // Update Title State
   const [updatedTitle, setUpdatedTitle] = useState("")
-  
+
+  // File Upload States
+  const [fileUpload, setFileUpload] = useState(null)
+
   useEffect(() => {
     getMovieList()
-  }, [] )
+  }, [])
 
   const getMovieList = async () => {
     try {
       const data = await getDocs(moviesCollectionRef);
-      const filteredData=data.docs.map((doc) => ({
-        ...doc.data(), 
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
         id: doc.id
       }));
       setMovieList(filteredData)
@@ -35,11 +39,12 @@ function App() {
   }
 
   const onSubmitMovie = async () => {
-    try{
-      await addDoc(moviesCollectionRef,{
-        title: newMovieTitle, 
-        releaseDate: newReleaseDate, 
-        recievedAnOscar: isNewMovieOscar 
+    try {
+      await addDoc(moviesCollectionRef, {
+        title: newMovieTitle,
+        releaseDate: newReleaseDate,
+        recievedAnOscar: isNewMovieOscar,
+        userId: auth?.currentUser.uid
       });
       getMovieList()
     } catch (err) {
@@ -48,32 +53,52 @@ function App() {
   };
 
   const deleteMovie = async (id) => {
-    const movieDoc = doc(db, "movies", id )
-    await deleteDoc(movieDoc)
-    getMovieList()
+    try {
+      const movieDoc = doc(db, "movies", id);
+      await deleteDoc(movieDoc)
+      getMovieList()
+    } catch (err) {
+      console.error(err)
+      alert("Error deleting movie")
+    }
+
   }
+
   const updateMovieTitle = async (id) => {
-    const movieDoc = doc(db, "movies", id )
-    await deleteDoc(movieDoc)
+    const movieDoc = doc(db, "movies", id)
+    await updateDoc(movieDoc, {
+      title: updatedTitle
+    })
     getMovieList()
   }
-  
+
+  const uploadFile = async () => {
+    if (!fileUpload) return;
+    const filesFolderRef = ref(storage, `moviesFolder/${fileUpload.name}`);
+    try {
+      await uploadBytes(filesFolderRef, fileUpload)
+    } catch (err) {
+      console.error(err)
+    }
+
+  }
+
   return (
     <div className="App">
       <Auth />
 
       <div>
-        <input 
-          placeholder="Movie Title..." 
+        <input
+          placeholder="Movie Title..."
           onChange={(e) => setNewMovieTitle(e.target.value)}
         />
-        <input 
-          placeholder="Release Date..." 
-          type="number" 
+        <input
+          placeholder="Release Date..."
+          type="number"
           onChange={(e) => setNewReleaseDate(Number(e.target.value))}
         />
-        <input 
-          type="checkbox" 
+        <input
+          type="checkbox"
           checked={isNewMovieOscar}
           onChange={(e) => setIsNewMovieOscar(e.target.checked)} />
         <label> Recieved and Oscar </label>
@@ -87,14 +112,24 @@ function App() {
             <label>Release Date: {movie.releaseDate} </label>
             <button onClick={() => deleteMovie(movie.id)}>Delete Movie</button>
 
-            <input 
-              placeholder="newTitle..."/>
-            <button> Update Title </button>
+            <input
+              placeholder="newTitle..."
+              onChange={(e) => setUpdatedTitle(e.target.value)}
+            />
+            <button onClick={() => updateMovieTitle(movie.id)} > Update Title </button>
           </div>
         ))}
       </div>
 
-      
+      <div>
+        <input
+          type="file"
+          onChange={(e) => setFileUpload(e.target.files[0])}
+        />
+        <button onClick={uploadFile}>Upload File</button>
+      </div>
+
+
     </div>
   );
 }
--- src/componets/auth.js
+++ src/componets/auth.js
@@ -6,7 +6,8 @@ export const Auth = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    console.log(auth?.currentUser?.email);
+    // console.log(auth?.currentUser?.email);
+    // console.log(auth?.currentUser?.uid);
 
     const signIn = async () => {
         try {
